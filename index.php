03c2dcb98cda78710a0731611f0ef15af41e304a
Gabriel Martins <gabrielmatheus209@gmail.com>
2021-07-05 14:22:54 -0300

    Adicionando exibicao de mensagem de retorno

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ini_set('display_errors', 0);
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +17,17 @@
   <p>Formulario Para Incriçao De Competidores</p>
 
   <form action="script.php" method="POST">
-    <p>Seu Nome: <input type="text" name="nome" required /></p>
+    <?php
+    $mensagemDeSucesso = isset($_SESSION['mensagem de sucesso']) ? $_SESSION['mensagem de sucesso'] : '';
+    if (!empty($mensagemDeSucesso)) {
+      echo $mensagemDeSucesso;
+    }
+    $mensagemDeErro = isset($_SESSION['mensagem de erro']) ? $_SESSION['mensagem de erro'] : '';
+    if (!empty($mensagemDeErro)) {
+      echo $mensagemDeErro;
+    }
+    ?>
+    <p>Seu Nome: <input type="text" name="nome" /></p>
     <p>Sua Idade: <input type="text" name="idade" /></p>
     <p><input type="submit" value="Enviar dados do competidor" /></p>
   </form>
