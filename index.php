b0a4b011fa3270f572184f656a90466db7b4e4a0
omartins-zs <gabrielmatheus209@gmail.com>
2023-01-07 20:26:39 -0300

    Ajustando Erros e CSS

--- index.php
+++ index.php
@@ -1,37 +1,55 @@
 <?php
 ini_set('display_errors', 0);
 session_start();
+include 'services/serviceMensagemSession.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="css/bulma.css">
-  <title>Formulario de inscricao</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/css/bulma.min.css">
+    <title>Formulario de inscricao</title>
 </head>
 
 <body>
-  <p>Formulario Para Incriçao De Competidores</p>
 
-  <form action="script.php" method="POST">
-    <?php
-    $mensagemDeSucesso = isset($_SESSION['mensagem de sucesso']) ? $_SESSION['mensagem de sucesso'] : '';
-    if (!empty($mensagemDeSucesso)) {
-      echo $mensagemDeSucesso;
-    }
-    $mensagemDeErro = isset($_SESSION['mensagem de erro']) ? $_SESSION['mensagem de erro'] : '';
-    if (!empty($mensagemDeErro)) {
-      echo $mensagemDeErro;
-    }
-    ?>
-    <p>Seu Nome: <input type="text" name="nome" /></p>
-    <p>Sua Idade: <input type="text" name="idade" /></p>
-    <p><input type="submit" value="Enviar Dados do competidor" /></p>
-  </form>
+
+    <div class="container">
+        <h5 class="title is-5">Formulario Para Incriçao De Competidores</h5>
+
+        <form class="form" action="script.php" method="POST">
+            <?php
+            $mensagemDeSucesso = obterMensagemSucesso();
+            if (!empty($mensagemDeSucesso)) {
+                echo $mensagemDeSucesso;
+            }
+            $mensagemDeErro = obterMensagemErro();
+            if (!empty($mensagemDeErro)) {
+                echo $mensagemDeErro;
+            }
+            ?>
+
+            <div class="field">
+                <label class="label"> </label>
+                <div class="control">
+                    <p>Seu Nome: <input class="input is-primary" type="text" name="nome" placeholder="Digite seu nome" /></p>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label"> </label>
+                <div class="control">
+                    <p>Sua Idade: <input class="input is-primary" type="text" name="idade" placeholder="Digite seu nome" /></p>
+                </div>
+            </div>
+            <p><input class="button" type="submit" value="Enviar Dados do competidor" /></p>
+        </form>
+
+    </div>
 </body>
 
 </html>
--- script.php
+++ script.php
@@ -1,63 +1,19 @@
 <?php
 ini_set('display_errors', 0);
 session_start();
-
-$categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
-$categorias[] = 'Idoso';
+include 'services/serviceMensagemSession.php';
+include 'services/serviceValidation.php';
+include 'services/serviceCategoriaCompetidor.php';
 
 // print_r($categorias);
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('Location: index.php');
+
 // var_dump($nome);
 // var_dump($idade);
 // return 0;
-
-if (empty($nome)) {
-    $_SESSION['mensagem de erro'] = 'O nome nao pode ser vazio, por favor preencha com seus dados';
-    header('Location: index.php');
-    return;
-} else if (strlen($nome) < 3) {
-    $_SESSION['mensagem de erro'] = 'O nome deve conter mais de 3 caracteres';
-    header('Location: index.php');
-    return;
-} else if (strlen($nome) > 40) {
-    $_SESSION['mensagem de erro'] = 'O nome e muito extenso';
-    header('Location: index.php');
-    return;
-} else if (!is_numeric($idade)) {
-    $_SESSION['mensagem de erro'] = 'Informe um numero para a sua idade';
-    header('Location: index.php');
-    return;
-}
-
-
-
-if ($idade >= 6 && $idade <= 12) {
-    for ($i = 0; $i <= count($categorias); $i++) {
-        if ($categorias[$i] == 'Infantil') {
-            $_SESSION['mensagem de sucesso'] = "O Nadador "  .  $nome .  " compete na categoria Infantl " .  $categorias[$i];
-            header('Location: index.php');
-            return;
-        }
-    }
-} else if ($idade >= 13 && $idade <= 18) {
-    for ($i = 0; $i <= count($categorias); $i++) {
-        if ($categorias[$i] == 'Adolescentes') {
-            $_SESSION['mensagem de sucesso'] = "O Nadador " .  $nome .  " compete na categoria" .  $categorias[$i];
-            header('Location: index.php');
-            return;
-        }
-    }
-} else {
-    for ($i = 0; $i <= count($categorias); $i++) {
-        if ($categorias[$i] == 'Adulto'){
-            $_SESSION['mensagem de sucesso']  = "O Nadador " . $nome .  " compete na categoria Adulto";
-            header('Location: index.php');
-            return;
-        }  
-    }
-}
